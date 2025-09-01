11f0c4cfb2b7b224e744ba8bf59a93136fd55363
jay menorca <zaimenorca@gmail.com>
2025-08-31 21:11:55 -0400

    section5; created consumer.Digital/PhysicalDeliverConsumer with SC Function physical/digitalDeliveryConsumer():Function<Flux<Message<Physical/DigitalDelivery>>, Mono<Void>>; and printMsgDetails(CustomRecord<Physical/DigitalDelevery>); probably can be refactored to have BaseDeliveryConsumer

---
+++ scs-kafka-sandbox/src/main/java/jayslabs/kafka/section5/consumer/DigitalDeliveryConsumer.java
@@ -0,0 +1,36 @@
+package jayslabs.kafka.section5.consumer;
+
+import java.util.function.Function;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+
+import jayslabs.kafka.common.CustomRecord;
+import jayslabs.kafka.common.MessageConverter;
+import jayslabs.kafka.section5.dto.DigitalDelivery;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class DigitalDeliveryConsumer {
+
+    private static final Logger log = LoggerFactory.getLogger(DigitalDeliveryConsumer.class);
+
+    @Bean
+    public Function<Flux<Message<DigitalDelivery>>, Mono<Void>> digitalDeliveryConsumer(){
+        return flux -> flux
+            .map(MessageConverter::toRecord)
+            .doOnNext(this::printMsgDetails)
+            .then();
+
+    }
+
+    private void printMsgDetails(CustomRecord<DigitalDelivery> rec){
+        log.info("Digital Consumer: {}", rec.message());
+        log.info("key: {}", rec.key());
+        rec.acknowledgement().acknowledge();
+    }
+}
---
+++ scs-kafka-sandbox/src/main/java/jayslabs/kafka/section5/consumer/PhysicalDeliveryConsumer.java
@@ -0,0 +1,36 @@
+package jayslabs.kafka.section5.consumer;
+
+import java.util.function.Function;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+
+import jayslabs.kafka.common.CustomRecord;
+import jayslabs.kafka.common.MessageConverter;
+import jayslabs.kafka.section5.dto.PhysicalDelivery;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class PhysicalDeliveryConsumer {
+
+    private static final Logger log = LoggerFactory.getLogger(PhysicalDeliveryConsumer.class);
+
+    @Bean
+    public Function<Flux<Message<PhysicalDelivery>>, Mono<Void>> physicalDeliveryConsumer(){
+        return flux -> flux
+            .map(MessageConverter::toRecord)
+            .doOnNext(this::printMsgDetails)
+            .then();
+
+    }
+
+    private void printMsgDetails(CustomRecord<PhysicalDelivery> rec){
+        log.info("Physical Consumer: {}", rec.message());
+        log.info("key: {}", rec.key());
+        rec.acknowledgement().acknowledge();
+    }
+}
