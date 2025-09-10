3b9a5f8cbcd1186feab7a037993a463ca28dc6dd
jay menorca <zaimenorca@gmail.com>
2025-09-09 21:39:52 -0400

    wrote a tool to route a message to Dead-Letter-Topic (DLT) on exception side-effect. Exception side effect is handled via reactive error recovery via .onErrorResume() and Mono.fromRunnable(), which calls handleError() - which writes the message to DLT

---
+++ scs-kafka-sandbox/src/main/java/jayslabs/kafka/section7/CharFinder.java
@@ -0,0 +1,53 @@
+package jayslabs.kafka.section7;
+
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Function;
+
+import jayslabs.kafka.common.CustomRecord;
+import jayslabs.kafka.common.MessageConverter;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.messaging.Message;
+import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.kafka.support.KafkaHeaders;
+
+@Configuration
+public class CharFinder {
+
+    private static final Logger log = LoggerFactory.getLogger(CharFinder.class);
+
+    @Autowired
+    private StreamBridge streamBridge;
+    
+    private static final String DLT_CHARFINDER_TOPIC = "dlt-charfinder-topic";
+
+    @Bean 
+    public Function<Flux<Message<String>>, Flux<Character>> processor(){
+        return flux -> flux
+            .map(MessageConverter::toRecord)
+            .concatMap(r -> this.find(r.message())
+                            .onErrorResume(ex -> Mono.fromRunnable(() -> this.handleError(ex, r)))
+                            .doAfterTerminate(() -> r.acknowledgement().acknowledge())
+        );
+    }
+
+    private Mono<Character> find(String message){
+        return Mono.just(message).map(m -> m.charAt(4));
+
+    }
+
+    private void handleError(Throwable ex, CustomRecord<String> record){
+        log.error(ex.getMessage());
+        this.streamBridge.send(DLT_CHARFINDER_TOPIC, 
+            MessageBuilder.withPayload(record.message())
+            .setHeader(KafkaHeaders.KEY, record.key())
+            .build()
+        );
+    }
+}
