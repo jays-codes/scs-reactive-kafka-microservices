74713e4059cf1e5b03f5a64035f75159095fa1d0
jay menorca <zaimenorca@gmail.com>
2025-11-09 21:33:22 -0500

    added CustomRecord (record) and MessageConverter to common.util

---
+++ saga-choreo/choreo-common/src/main/java/jayslabs/kafka/common/util/MessageConverter.java
@@ -0,0 +1,17 @@
+package jayslabs.kafka.common.util;
+
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+
+import reactor.kafka.receiver.ReceiverOffset;
+
+public class MessageConverter {
+
+    public static <T> CustomRecord<T> toRecord(Message<T> message){
+        var payload = message.getPayload();
+        var key = message.getHeaders().get(KafkaHeaders.RECEIVED_KEY, String.class);
+        var ack = message.getHeaders().get(KafkaHeaders.ACKNOWLEDGMENT, ReceiverOffset.class);
+
+        return new CustomRecord<>(key, payload, ack);
+    }
+}
