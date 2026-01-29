cf5fdd8d496549d300b20e40b7eefe8e91358212
Jay Menorca <zaimenorca@gmail.com>
2026-01-29 11:49:26 -0500

    updated OrderServiceTest with compensatingTransactionTest() - where PaymentFailed event sent but Inventory already deducted. Expected: OrderStatus.CANCELLED, PaymentStatus.FAILED, InventoryStatus.RESTORED

--- saga-choreo/order-service/src/test/java/jayslabs/kafka/order/OrderServiceTest.java
+++ saga-choreo/order-service/src/test/java/jayslabs/kafka/order/OrderServiceTest.java
@@ -95,4 +95,44 @@ public class OrderServiceTest extends AbstractIntegrationTest{
             Assertions.assertEquals(InventoryStatus.DECLINED, odto.inventory().status());
         });
     }
+
+    @Test
+    public void compensatingTransactionTest() throws InterruptedException{
+
+        // simulate OrderCreateRequest sent from OrderController
+        var req = TestDataUtil.toOrderCreateRequest(1, 1, 2, 3);
+        
+        // validate order in pending state
+        var ordId = initiateOrder(req);
+
+        // check for OrderCreated event
+        verifyOrderCreatedEvent(ordId, 6);
+
+        // simulate Payment Declined event sent from PaymentService
+        emitEvent(PaymentEvent.PaymentFailed.builder().orderId(ordId).build());
+
+        // simulate InventoryDeducted event sent from InventoryService
+        emitEvent(InventoryEvent.InventoryDeducted.builder().orderId(ordId).build());
+
+        // check for OrderCancelled event
+        verifyOrderCancelledEvent(ordId);
+
+        // emit inventory restored event
+        emitEvent(InventoryEvent.InventoryRestored.builder().orderId(ordId).build());
+
+        // verify order details via REST endpoint
+        // we might have to wait for sometime for streambridge to send and app to process
+        Thread.sleep(1_500);
+
+        //this.expectNoEvent();
+
+
+        // order details should not have delivery date
+        verifyOrderDetails(ordId, odto -> {
+            Assertions.assertNull(odto.order().deliveryDate());
+            Assertions.assertEquals(OrderStatus.CANCELLED, odto.order().status());
+            Assertions.assertEquals(PaymentStatus.FAILED, odto.payment().status());
+            Assertions.assertEquals(InventoryStatus.RESTORED, odto.inventory().status());
+        });
+    }
 }
